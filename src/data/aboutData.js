fa31d9cc2a95ede5548a228dc4cd186c24e4b675
Pradyumn-Patil <pradyumn.narendra.patil@gmail.com>
2021-11-28 08:51:26 +0530

    theme and about changed

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,12 +1,8 @@
 export const aboutData = {
-<<<<<<< HEAD
+
     title: "About Myself",
     description1: "My name is Pradyumn Narendra Patil . I'm a Computer-Science student at Vishwakarma Insititute of technology , Pune ",
     description2: "",
-=======
-    title: "About Myself ",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
->>>>>>> 288d3626b007d4444a587f1243173c75fcff872b
+
     image: 2
 }
--- src/data/themeData.js
+++ src/data/themeData.js
@@ -3,13 +3,10 @@ import {
     greenThemeLight, greenThemeDark, bwThemeLight, bwThemeDark, blueThemeLight, blueThemeDark, redThemeLight, redThemeDark, orangeThemeLight, orangeThemeDark, purpleThemeLight, purpleThemeDark, pinkThemeLight, pinkThemeDark, yellowThemeLight, yellowThemeDark
 } from '../theme/theme'
 
-
 export const themeData = {
-<<<<<<< HEAD
+
     theme: blueThemeLight
-=======
-    theme: blueThemeDark
->>>>>>> 288d3626b007d4444a587f1243173c75fcff872b
+
 }
 
 
